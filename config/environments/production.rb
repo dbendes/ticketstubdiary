78a8ee2364c0bfcc889132bf9b1be574a67e2301
dbendes <david.bendes+github@gmail.com>
2021-10-07 11:38:24 -0400

    fixing production application.css routing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # make sure the assets exists - might have been busted during the push to production?
+  config.assets.digest = true
 end
